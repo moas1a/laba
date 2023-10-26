72e19af956596be95de6f5aa08fa4c2986c614fe
Leonid <smolyanov.lm@gmail.com>
2023-10-26 20:59:25 +0300

    new

--- laba/Program.cs
+++ laba/Program.cs
@@ -7,6 +7,8 @@ namespace MyApp // Note: actual namespace depends on the project name.
         static void Main(string[] args)
         {
             Console.WriteLine("kk");
+            Console.WriteLine("qq ww")
         }
     }
 }
+
