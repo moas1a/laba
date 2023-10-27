02f8267f00ac05b7eccecc44a3238b92c6f42418
Gosha <shchegl00v@yandex.ru>
2023-10-27 19:29:42 +0300

    Всё плохо

--- laba/Program.cs
+++ laba/Program.cs
@@ -1,13 +1,69 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
 
-class Player {
+public static class JsonHelper
+{
+    public static void SavePlayersToFile(List<Player> players, string filePath)
+    {
+        try
+        {
+            var json = JsonConvert.SerializeObject(players, Formatting.Indented);
+            File.WriteAllText(filePath, json);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Ошибка при сохранении данных: " + ex.Message);
+        }
+    }
+    public static List<Player> LoadPlayersFromFile(string filePath)
+    {
+        try
+        {
+            if (!File.Exists(filePath)) return new List<Player>();
+            var json = File.ReadAllText(filePath);
+            return JsonConvert.DeserializeObject<List<Player>>(json) ?? new List<Player>();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Ошибка при загрузке данных: " + ex.Message);
+            return new List<Player>();
+        }
+    }
+    public static void SaveDictionaryToJsonFile(Dictionary<string, string> dictionary, string filePath)
+    {
+        try
+        {
+            // Конвертируем словарь в строку JSON
+            string json = JsonConvert.SerializeObject(dictionary, Formatting.Indented);
+
+            // Сохраняем JSON строку в файл
+            File.WriteAllText(filePath, json);
+
+            Console.WriteLine("Данные успешно сохранены в файл: " + filePath);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Возникла ошибка при сохранении данных: " + ex.Message);
+        }
+    }
+}
+public class Player {
     public string Name { get; set; }
     public string Password { get; set; }
-
+    public int TotalMoves { get; set; }
+    public int WinsCount { get; set; }
     public Player(string name, string password) {
         Name = name;
         Password = password;
+        TotalMoves = 0;
+        WinsCount = 0;
+    }
+    public float AverageMovesPerWin()
+    {
+        if (WinsCount == 0) return TotalMoves;
+        return (float) TotalMoves / WinsCount;
     }
 }
 
@@ -19,6 +75,7 @@ class Program {
     static List<Player> players = new List<Player>();
 
     static void Main(string[] args) {
+        var players = JsonHelper.LoadPlayersFromFile("players.json");  // загружаем данные
         Console.WriteLine("Добро пожаловать в игру Ним!");
 
         bool gameRunning = true;
@@ -29,16 +86,21 @@ class Program {
             Console.WriteLine("2. Новый игрок");
             Console.WriteLine("3. Выйти из игры");
 
-            int choice = GetValidInput(1, 3);
+            int choice = GetValidInput(1, 4);
 
             switch (choice) {
                 case 1:
                     PlayGame();
+                    JsonHelper.SavePlayersToFile(players, "players.json");  // сохраняем данные
                     break;
                 case 2:
                     CreateProfile();
+                    JsonHelper.SavePlayersToFile(players, "players.json");  // сохраняем данные
                     break;
                 case 3:
+                    ShowLeaderboard();
+                    break;
+                case 4:
                     Console.WriteLine("Выход из игры.");
                     gameRunning = false;
                     break;
@@ -47,7 +109,22 @@ class Program {
 
         Console.ReadLine();
     }
-
+    
+    static void ShowLeaderboard()
+    {
+        if (players.Count == 0)
+        {
+            Console.WriteLine("Нет доступных профилей.");
+            return;
+        }
+        var sortedPlayers = players.OrderBy(p => p.AverageMovesPerWin()).ToList();
+        Console.WriteLine("\nТаблица лидеров:");
+        for (var i = 0; i < sortedPlayers.Count; i++)
+        {
+            Console.WriteLine($"{i + 1}. {sortedPlayers[i].Name} - среднее число ходов за игру: {sortedPlayers[i].AverageMovesPerWin()}");
+        }
+    }
+    
     static void PlayGame() {
         if (players.Count == 0) {
             Console.WriteLine("Нет доступных профилей. Создайте новый профиль.");
@@ -74,7 +151,9 @@ class Program {
         currentPlayer = 1;
 
         Console.WriteLine("\nИгра началась!");
-
+        
+        int playerMoves = 0;
+        
         while (sticks > 0) {
             Console.WriteLine("\nНа столе осталось " + sticks + " палочек.");
 
@@ -85,12 +164,18 @@ class Program {
                 sticksTaken = GetComputerMove();
                 Console.WriteLine("Ход компьютера: " + sticksTaken);
             }
+            if (currentPlayer == 1) 
+            {
+                playerMoves++;  // увеличиваем количество ходов игрока
+            }
 
             sticks -= sticksTaken;
 
             if (sticks <= 0) {
                 if (currentPlayer == 1) {
                     Console.WriteLine("Поздравляем! Вы победили!");
+                    players[profileIndex].WinsCount++;  // увеличиваем количество побед игрока
+                    players[profileIndex].TotalMoves += playerMoves;  // добавляем количество ходов
                 } else {
                     Console.WriteLine("К сожалению, компьютер победил.");
                 }
